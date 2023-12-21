ca7c220b256d22858c45e04a9180c927b91de98f
tanushukla1332 <tanushukla1332@gmail.com>
2023-12-21 18:31:26 +0530

    remove un

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -5,37 +5,8 @@ import Col from 'react-bootstrap/Col';
 
 export default function Login() {
     return (
-        <Container fluid >
-            <Row>
-                <Col md={5} className='mb-4'>
-                    <Card>
-                        <CardImg src='https://img.freepik.com/premium-photo/3d-illustration-purple-retro-headphones-purple-isolated-background-white-lights-headphone-icon-illustration_116124-7777.jpg' alt='Image alt text'  />
-                        <CardBody>
-                            <CardSubtitle className='mb-2 text-muted'>Card subtitle</CardSubtitle>
-                            <CardText>
-                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus, eligendi.
-                            </CardText>
-                            <CardLink href='#'>Card link</CardLink>
-                            <Button variant='outline-primary' className='ml-2'>
-                                Read more
-                            </Button>
-                        </CardBody>
-                    </Card>
-                </Col>
-                <Col md={5} className='mb-5'>
-                    <Card>
-                        <CardImg src='https://img.freepik.com/premium-photo/3d-illustration-purple-retro-headphones-purple-isolated-background-white-lights-headphone-icon-illustration_116124-7777.jpg' fluid  />
-                        <CardBody>
-                            <CardSubtitle className="mb-2 text-muted">Card subtitle</CardSubtitle>
-                            <CardText>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus, eligendi.</CardText>
-                            <CardLink href='#'>Card link</CardLink>
-                            <Button variant='outline-primary' className='ml-2'>
-                                Read more
-                            </Button>
-                        </CardBody>
-                    </Card>
-                </Col>
-            </Row>
-        </Container>
+       <>
+       <h1>Login page </h1>
+       </>
     );
 }
